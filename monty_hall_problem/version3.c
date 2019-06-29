35b88ed18a1ef65a0fac01d3b234a03e626ef9e3
MoriGM <30366747+MoriGM@users.noreply.github.com>
2019-06-29 14:49:30 +0200

    Fixed a Problemm and added a new Version

--- monty_hall_problem/version3.c
+++ monty_hall_problem/version3.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
+int get_hall(int arr[], int pick)
+{
+	for (int i = 0;i < 3;i++)
+		if (i != pick && arr[i] == 0)
+			return i;
+	return 0;
+}
+
 int main(void)
 {
 	int number = 0, result[] = {0, 0}; // result 0 = wrong, 1 = right
@@ -14,13 +22,13 @@ int main(void)
 	{
 		int arr[3] = {0, 0, 0};
 		arr[rand() % 3] = 1;
-		int pick = rand() % 3, monty = rand() % 2;
-		int pos = 3 - ((pick + monty) % 3);
+		int pick = rand() % 3, monty = get_hall(arr, pick);
+		int pos = 3 - (((pick + 1) + (monty + 1)) % 3);
 		result[arr[pos - 1]] += 1;
 	}
 
 	printf("Right: %d and Wrong: %d\n", result[1], result[0]);
-	printf("You woud win %d%% times and lose %d%% times\n", ((result[0] * 100) / number), ((result[1] * 100) / number));
+	printf("You woud win %d%% times and lose %d%% times\n", ((result[1] * 100) / number), ((result[0] * 100) / number));
 	
 	return 0;
 }
