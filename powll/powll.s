75f944cfbc192e2a95e397580e9d2d948e1eb2d4
MoriGM <30366747+MoriGM@users.noreply.github.com>
2019-10-27 19:09:06 +0100

    Edit Code in powll

--- powll/powll.s
+++ powll/powll.s
@@ -4,6 +4,8 @@
 powll:
 	push %rsi
 	mov $1, %rax
+	xor $0, %rsi
+
 	jmp powll_LC100
 
 powll_LC0:
@@ -11,8 +13,7 @@ powll_LC0:
 	mul %rdi
 
 powll_LC100:
-	cmp $0, %rsi
-	jne powll_LC0
+	jnz powll_LC0
 
 	pop %rsi	
 
